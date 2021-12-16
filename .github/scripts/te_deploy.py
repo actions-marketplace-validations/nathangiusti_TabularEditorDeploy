9ab7c1638536665003b121d63d892845ebe7e331
Nathan Giusti <bigbadbyte2@gmail.com>
2021-12-16 19:56:13 +0000

    Break single string param into files

--- .github/scripts/te_deploy.py
+++ .github/scripts/te_deploy.py
@@ -8,11 +8,13 @@ import zipfile
 def main():
 
     arg_list = []
-    print("Argument: {}".format(sys.argv))
-    print("{} arguments".format(len(sys.argv)))
-    for arg in sys.argv:
-        print(arg)
-        
+
+    file_string = sys.argv[1]
+    file_list = file_string.split(',')
+    for file in file_list:
+        print(file)
+
+
     # Download Tabular Editor Portable, extract, and delete zip file
     cwd = os.getcwd()
     zip_location = cwd + "\TabularEditor.zip"
