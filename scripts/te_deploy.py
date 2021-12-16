e00e2b550fd82ecc7cf048ec30d05c7405d4280b
Nathan Giusti <bigbadbyte2@gmail.com>
2021-12-16 16:28:41 +0000

    Action test

---
+++ scripts/te_deploy.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import sys
+import urllib.request
+import zipfile
+
+
+def main():
+
+    # Download Tabular Editor Portable, extract, and delete zip file
+    cwd = os.getcwd()
+    zip_location = cwd + "\TabularEditor.zip"
+    unzip_location = cwd + "\TabularEditor.exe"
+    urllib.request.urlretrieve("https://cdn.tabulareditor.com/files/te2/TabularEditor.Portable.zip", cwd + "\TabularEditor.zip")
+
+    with zipfile.ZipFile(zip_location, 'r') as zip_ref:
+        zip_ref.extractall(cwd)
+
+    os.remove(zip_location)
+    
+    # Find all modified datasets
+    updated_datasets = []
+    for file in sys.argv:
+        if os.path.exists(file):
+            path = Path(file)
+            dataset = workspace = os.path.basename(path.parent.absolute())
+            if dataset not in updated_datasets:
+                updated_datasets.append(dataset)
+    
+    print(updated_datasets)
+
+    os.system()
