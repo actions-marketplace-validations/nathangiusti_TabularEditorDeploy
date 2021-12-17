980a8f48bd5edbb9926cfc7caa62adcf61310418
Nathan Giusti <bigbadbyte2@gmail.com>
2021-12-17 15:35:12 -0500

    Fixed issue with incoming files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pip install --upgrade pip
 pip install requests
-python /scripts/te_deploy.py --files $1 --tenant_id $2 --db_url $3
+python /scripts/te_deploy.py --files "$1" --tenant_id $2 --db_url $3
